97c2d77106500a42cafa664182a8affa15649357
Lennox Mountain <lennoxmountain67@gmail.com>
2024-04-19 22:08:58 +0200

    added another file

--- B_initialsPrint.cpp
+++ B_initialsPrint.cpp
@@ -2,8 +2,7 @@
 #include <cstring>
 using namespace std;
 
-// the algorithm works but it is not memory efficient. 
-
+// the algorithm works but it is not memory efficient.
 void b_stringPrint(){
     string word[] = {"B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"};
     //the size variable has been given type long to avoid overflow. 

ba4ca72a372d23d9891f1d3cbda5961786c933d1
Lennox Mountain <lennoxmountain67@gmail.com>
2024-04-19 21:10:04 +0200

    more and more errors

--- B_initialsPrint.cpp
+++ B_initialsPrint.cpp
@@ -4,11 +4,12 @@ using namespace std;
 
 void b_stringPrint(){
     string word[] = {"B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"};
+    long size = sizeof(word) / sizeof(word[0]);
     cout<<"words with initial of 'B' include: \n";
 
-    for (int i = 0; i < word->length(); i++){
+    for (long i = 0; i < size; i++){
         if (word[i].substr(0, 1) == "B"){
-            cout<<word[i]<<endl;
+            cout<<word<<endl;
         }
     }
 

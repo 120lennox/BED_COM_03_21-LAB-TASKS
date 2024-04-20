0b851c0ac89f98a97fa4556d8b2ab6284c3be514
Lennox Mountain <lennoxmountain67@gmail.com>
2024-04-20 08:07:19 +0200

    morning algorithms

--- number_range.cpp
+++ number_range.cpp
@@ -17,6 +17,10 @@ void inputNum() {
         if (std::is_same<input, string>::value){
             cout<<"Sorry, you entered an invalid number. Please try again."<<endl;
         }
+
+        else if(std::is_same<input, double>::value || std::is_same<input, float>::value){
+            cout<<"Sorry, you entered an invalid number. Please try again."<<endl;
+        }
         
         else if(std::is_same<input, int>::value){
             if(num >= 5 && num <=10){
@@ -30,6 +34,22 @@ void inputNum() {
         else{
             cout<<"Sorry, you entered an invalid number. Please try again."<<endl;
         }
+    }
+}
+
+void num_range(){
+    int num;
+    while (true){
+        try
+        {
+            cout<<"Please enter a number between 5 and 10: ";
+            cin>>num;
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+        
     }
 }
 

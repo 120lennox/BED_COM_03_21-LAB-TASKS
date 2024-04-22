11fb3143e48ca56d7f0eb1e042b2e2a413baf468
Lennox Mountain <lennoxmountain67@gmail.com>
2024-04-22 23:18:42 +0200

    tried different algorithm

---
+++ areaCalc.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int rectangle(int width, int length){
+    return length * width;
+}
+int square(int length){
+    return length * length;
+}
+
+int triangle(int base, int height){
+    return (base * height) / 2;
+}
+
+int main(){
+    
+}

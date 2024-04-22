d334f5327513573caba65739195a54c5db1c987d
Lennox Mountain <lennoxmountain67@gmail.com>
2024-04-22 21:52:40 +0200

    small error freaking me out

--- number_range.cpp
+++ number_range.cpp
@@ -1,61 +1,38 @@
 /**
  * @author: Lennox Mountain
+ * 
 */
+
 #include <iostream>
 #include <string>
-#include <type_traits>
 using namespace std;
 
-template <typename input>
+void input_num(){
+    string num;
+    int count = 0;
+    while(count == 1){
+        cout<<"Enter a value between 5 and 10: ";
+        getline(cin, num);
 
-void inputNum() {
-    input num{};
-    while (true){
-        cout<<"Please enter a number between 5 and 10: ";
-        cin>>num;
-
-        if (std::is_same<input, string>::value){
-            cout<<"Sorry, you entered an invalid number. Please try again."<<endl;
-        }
-
-        else if(std::is_same<input, double>::value || std::is_same<input, float>::value){
-            cout<<"Sorry, you entered an invalid number. Please try again."<<endl;
-        }
-        
-        else if(std::is_same<input, int>::value){
-            if(num >= 5 && num <=10){
-                cout<<"Your input ("<<num<<") has been accepted."<<endl;
-                break;
-            }
-            else{
-                cout<<"You entered "<<num<<". Please enter a number between 5 and 10."<<endl;
-            }
-        }
-        else{
-            cout<<"Sorry, you entered an invalid number. Please try again."<<endl;
-        }
-    }
-}
-
-void num_range(){
-    int num;
-    while (true){
         try
         {
-            cout<<"Please enter a number between 5 and 10: ";
-            cin>>num;
+            int number = stoi(num);
+            if (number >= 5 && number <=10){
+                cout<<"Your input("<<number<<") has been accepted"<<endl;
+                count++;
+            }
+
+            else{
+                cout<<"You entered "<<number<<". Please enter a value between 5 and 10 \n";
+            }
         }
         catch(const std::exception& e)
         {
-            std::cerr << e.what() << '\n';
+            std::cerr<<"Sorry you entered an invalid number. Please try again" << e.what() << '\n';
         }
-        
     }
 }
 
-
-
 int main(){
-    inputNum<int>();
-    return 0;
+    input_num();
 }

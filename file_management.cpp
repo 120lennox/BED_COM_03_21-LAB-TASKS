8088f0b7dfe8ac002f55af1902b30c11584ab184
Lennox Mountain <lennoxmountain67@gmail.com>
2024-05-05 23:43:54 +0200

    implemented calculate vowel successfully

--- file_management.cpp
+++ file_management.cpp
@@ -8,10 +8,19 @@
 using namespace std;
 
 int calculate_vowels(string fileData){
-    
+    int count = 0;
+    for(char c: fileData){
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'){
+            count++;
+        }
+    }
+
+    return count;
 }
 
-int number_of_words(string filedata);
+int number_of_words(string filedata){
+    int count = 0;
+}
 string reverse_string(string filedata);
 string capitalize_letter(string filedata);
 int main(){
@@ -26,7 +35,9 @@ int main(){
     if (input.fail()) cout<<"Error! file does not exist \n";
     else{
         getline(input, filedata);
-        cout<<filedata;
     }
+
+    int vowel_count = calculate_vowels(filedata);
+    cout<<vowel_count<<" vowels"<<endl;
 
 }

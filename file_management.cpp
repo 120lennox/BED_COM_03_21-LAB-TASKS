21b95e89d2b8bb118a297f32ee63b6843905da5c
Lennox Mountain <lennoxmountain67@gmail.com>
2024-05-06 00:20:53 +0200

    implementing main functions

--- file_management.cpp
+++ file_management.cpp
@@ -50,19 +50,24 @@ void capitalize_letter(string filedata){
     }
     
 }
+
+/**
+ * main: executes all functions defined
+ * return: 0 on success
+*/
 int main(){
+    //input objecting for reading files from external files
     ifstream input;
-    ofstream output;
 
     string filedata;
 
     input.open("fileData.txt");
-    output.open("output.txt");
 
     if (input.fail()) cout<<"Error! file does not exist \n";
     else{
         getline(input, filedata);
     }
+
 
     // int vowel_count = calculate_vowels(filedata);
     // cout<<vowel_count<<" vowels"<<endl;

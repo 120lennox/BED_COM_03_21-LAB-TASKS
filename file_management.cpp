65b82d68797b9e64312c559871281ee4bb642c35
Lennox Mountain <lennoxmountain67@gmail.com>
2024-05-05 23:31:46 +0200

    implemented file opening functionality

--- file_management.cpp
+++ file_management.cpp
@@ -7,11 +7,26 @@
 #include <string>
 using namespace std;
 
-int calculate_vowels(string fileData);
+int calculate_vowels(string fileData){
+    
+}
 
 int number_of_words(string filedata);
 string reverse_string(string filedata);
 string capitalize_letter(string filedata);
 int main(){
-    
+    ifstream input;
+    ofstream output;
+
+    string filedata;
+
+    input.open("fileData.txt");
+    output.open("output.txt");
+
+    if (input.fail()) cout<<"Error! file does not exist \n";
+    else{
+        getline(input, filedata);
+        cout<<filedata;
+    }
+
 }

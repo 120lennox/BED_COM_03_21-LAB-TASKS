0bff4b73f01de8fe24c2986b5a266ebc917cdbdd
Lennox Mountain <lennoxmountain67@gmail.com>
2024-05-15 23:49:31 +0200

    lab 7 working

---
+++ Lab 7/Rectangle.cpp
@@ -0,0 +1,21 @@
+#include "Rectangle.H"
+
+//class instantiation
+Rectangle::Rectangle(){
+    width = 0;
+    length = 0;
+}
+
+void Rectangle::set_legnth(float l){
+    length = l;
+}
+
+void Rectangle::set_width(float w){
+    width = w;
+}
+
+float Rectangle::calculate_area(float w, float l){
+    return w * l;
+}
+
+Rectangle::~Rectangle(){};
